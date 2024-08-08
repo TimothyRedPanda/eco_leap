6dd1661d96151aec7d817613fb27090a36938a15
Kels <kelsey.baker1960@gmail.com>
2024-08-08 15:46:22 +0100

    header, body and footer layout complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Eco Leap",
 	description:
-		"A travel carbon emissions calculator, to help you reduce your impact.",
+		"A travel carbon emissions calculator, to help you reduce your enviornmental impact.",
 };
 
 export default function RootLayout({
@@ -17,7 +19,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={`w-screen h-screen text-center overflow-hidden ${inter.className}`}>
+				<NavBar />
+
+				<main className="overflow-scroll">{children}</main>
+
+				<Footer />
+
+				</body>
 		</html>
 	);
 }
