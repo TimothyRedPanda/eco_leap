92d1e082c62774c0ebfa3ec822dfceabdf180251
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-08-08 18:24:02 +0100

    Favicon and Resized Icon

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export default function Navbar() { 
-    return (
-        <nav className="w-full h-30 flex flex-row gap-4 p-3 items-center shadow-navShadow">
-        <Image src="eco_leap_bob.svg" alt="Eco Leap Logo" width={48} height={48} />
-        <h1 className="font-bold text-xl">EcoLeap</h1>
-        </nav>
-    )
+export default function Navbar() {
+	return (
+		<nav className="w-full h-30 flex flex-row gap-4 p-3 items-center shadow-navShadow">
+			<Image
+				src="eco_leap_bob.svg"
+				alt="Eco Leap Logo"
+				width={42}
+				height={42}
+				priority={true}
+			/>
+			<h1 className="font-bold text-xl">EcoLeap</h1>
+		</nav>
+	);
 }
